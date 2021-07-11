30a497a15a105610563f1c7645ada23992ad381e
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-11 23:29:50 +0530

    [SaiVijayaKumar] Inserting value to Queue by Enqueue method

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -15,13 +15,13 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t----------------->WELCOME TO QUEUE OPERATIONS<----------------\n");
-            QueueMethods stack = new QueueMethods();
+            QueueMethods queue = new QueueMethods();
             Console.WriteLine("\tQueue Contains\n");
             //pushing values to stack
-            stack.Enqueue(56);
-            stack.Enqueue(30);
-            stack.Enqueue(70);
-            stack.DisplayList();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.DisplayList();
             Console.Read();
         }
     }
