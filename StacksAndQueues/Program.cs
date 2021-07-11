c907c8b12c4c029a5fe34c997f07ae8e42fbbbd0
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-11 22:25:54 +0530

    [SaiVijayaKumar] Commiting Welcome Message

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        /// <summary>
+        /// ----------------->WELCOME TO STACK OPERATIONS<----------------
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t----------------->WELCOME TO STACK OPERATIONS<----------------\n");
+            StackMethods stack = new StackMethods();
+            Console.WriteLine("\tStack Contains\n");
+            stack.PushToStack(70);
+            stack.PushToStack(30);
+            stack.PushToStack(56);
+            stack.DisplayList();
+            Console.Read();
+        }
+    }
+}
---
+++ StacksAndQueues/StackMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class StackMethods
+    {
+        public Node head;
+        public Node tail;
+        public void PushToStack(int data)
+        {
+            Node newnode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = this.tail = newnode;
+            }
+            else
+            {
+                //linking newnode to head
+                newnode.next = this.head;
+                //Updating head pointer
+                this.head = newnode;
+            }
+        }
+        public void DisplayList()
+        {
+            int count = 0;
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.WriteLine("{1} At {0} Index", count, temp.data);
+                count++;
+                temp = temp.next;
+            }
+        }
+    }
+}
